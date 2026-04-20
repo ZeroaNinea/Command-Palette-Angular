565e8d9e73316a788b5349510c32ec4436288c91
Catgirl <zeroaninea@gmail.com>
2026-04-20 23:34:39 +0400

    command palette imported

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommandPalette } from './command-palette/command-palette';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommandPalette],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Command-Palette-Angular');
