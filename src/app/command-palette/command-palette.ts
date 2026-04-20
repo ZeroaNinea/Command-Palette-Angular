cb1f34dae02e50fd6b483d99176efe4141a374d3
Catgirl <zeroaninea@gmail.com>
2026-04-20 21:40:13 +0400

    adding basic command functionality

--- src/app/command-palette/command-palette.ts
+++ src/app/command-palette/command-palette.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Command } from '../../types/command.alias';
 
 @Component({
   selector: 'app-command-palette',
@@ -6,4 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './command-palette.html',
   styleUrl: './command-palette.scss',
 })
-export class CommandPalette {}
+export class CommandPalette {
+  @Input() commands: Command[] = [];
+  @Output() commandSelected = new EventEmitter<Command>();
+
+  query = '';
+  filtered: Command[] = [];
+  activeIndex = 0;
+  isOpen = false;
+
+  filter() {
+    const q = this.query.toLowerCase();
+
+    this.filtered = this.commands.filter(
+      (cmd) => cmd.label.toLowerCase().includes(q) || cmd.keywords?.some((k) => k.includes(q)),
+    );
+
+    this.activeIndex = 0;
+  }
+
+  onCommand(cmd: Command) {
+    this.commandSelected.emit(cmd);
+    this.isOpen = false;
+  }
+}
