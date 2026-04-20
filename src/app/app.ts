cf01db9274f320e90d86a1601aea953166f672e6
Catgirl <zeroaninea@gmail.com>
2026-04-21 00:34:05 +0400

    color input component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommandPalette } from './command-palette/command-palette';
+import { ColorInput } from './color-input/color-input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommandPalette],
+  imports: [RouterOutlet, CommandPalette, ColorInput],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
